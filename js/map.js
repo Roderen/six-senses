3d3909f054f473211cce6ed05d6c4c7f33b4a0cc
Roderen <mikilikala@gmail.com>
2022-08-10 14:42:19 +0300

    update

---
+++ js/map.js
@@ -0,0 +1,232 @@
+// Initialize and add the map
+function initMap() {
+    const infoWindow = new google.maps.InfoWindow({
+        content: "",
+        disableAutoPan: true,
+    });
+    // The location of "Palm Jumeirah"
+    const PalmJumeirah = { lat: 25.102, lng: 55.115 };
+    const myStyles = [
+        {
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#f5f5f5"
+            }
+          ]
+        },
+        {
+          "elementType": "geometry.fill",
+          "stylers": [
+            {
+              "color": "#f5f5f5"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.icon",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#858585"
+            }
+          ]
+        },
+        {
+          "elementType": "labels.text.stroke",
+          "stylers": [
+            {
+              "color": "#f5f5f5"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "administrative.land_parcel",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#bdbdbd"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#eeeeee"
+            }
+          ]
+        },
+        {
+          "featureType": "poi",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#757575"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#e5e5e5"
+            }
+          ]
+        },
+        {
+          "featureType": "poi.park",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#9e9e9e"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#ffffff"
+            }
+          ]
+        },
+        {
+          "featureType": "road",
+          "elementType": "labels.icon",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "road.arterial",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#757575"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#dadada"
+            }
+          ]
+        },
+        {
+          "featureType": "road.highway",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#616161"
+            }
+          ]
+        },
+        {
+          "featureType": "road.local",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#9e9e9e"
+            }
+          ]
+        },
+        {
+          "featureType": "transit",
+          "stylers": [
+            {
+              "visibility": "off"
+            }
+          ]
+        },
+        {
+          "featureType": "transit.line",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#e5e5e5"
+            }
+          ]
+        },
+        {
+          "featureType": "transit.station",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#eeeeee"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "geometry",
+          "stylers": [
+            {
+              "color": "#EDEDED"
+            }
+          ]
+        },
+        {
+          "featureType": "water",
+          "elementType": "labels.text.fill",
+          "stylers": [
+            {
+              "color": "#9e9e9e"
+            }
+          ]
+        }
+      ]
+    // The map, centered at Palm Jumeirah
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 13,
+        center: PalmJumeirah,
+        styles: myStyles,
+    });
+    // The marker, positioned at PalmJumeirah
+    const marker = new google.maps.Marker({
+        position: PalmJumeirah,
+        map: map,
+    });
+}
+
+window.initMap = initMap;
---
+++ js/script.js
@@ -0,0 +1,40 @@
+// Floor tabs
+function floorsTabs() {
+  let tabsButton = document.querySelectorAll('.floors__btn');
+  let tabsBlock = document.querySelectorAll('.floors__block');
+
+  let counter = 0;
+
+  tabsButton.forEach((btn, index) => {
+    btn.addEventListener('click', () => {
+      activeButton(btn);
+      btn.classList.add('floors__btn-active');
+      updateContent(index);
+    })
+  })
+
+  function activeButton() {
+    tabsButton.forEach((btn) => {
+      btn.classList.remove('floors__btn-active');
+    })
+  }
+
+  function updateContent(index) {
+    counter = index
+    updateActiveElements();
+  }
+
+  function updateActiveElements() {
+    tabsBlock.forEach(block => {
+      block.classList.contains('floors__block-active') && block.classList.remove('floors__block-active')
+    })
+    tabsBlock[counter].classList.add('floors__block-active')
+
+    tabsButton.forEach((btn) => {
+      btn.classList.contains('floors__btn-active') && btn.classList.remove('floors__btn-active')
+    })
+    tabsButton[counter].classList.add('floors__btn-active');
+  }
+  updateActiveElements()
+}
+floorsTabs()
