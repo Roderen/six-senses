1b9f2dd63d8092e66fdca5faed1c6bc82d16e796
Roderen <mikilikala@gmail.com>
2022-08-11 03:29:52 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,25 @@
+var controller = new ScrollMagic.Controller();
+
+$(".trigger-line").each(function () {
+  var tl = new TimelineMax();
+  var child = $(this).find(".all-line");
+  tl.to(child, 1, { height: 200, ease: Linear.easeNone });
+
+  var scene = new ScrollMagic.Scene({
+    triggerElement: this,
+    triggerHook: 0.4,
+    duration: "100%"
+  })
+    .setTween(tl)
+    // .addIndicators({
+    //   colorTrigger: "black",
+    //   colorStart: "black",
+    //   colorEnd: "black",
+    //   indent: 10
+    // })
+    .addTo(controller);
+});
+
 
 
 
